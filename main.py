c794a82c2ca12b1b185d9065e37d31823a9c7e26
tharrmeehan <tharrmeehan@icloud.com>
2024-08-18 20:39:29 +0200

    added initial court drawing (W.I.P.)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
 from court_line_detector import CourtLineDetector
+from mini_court import MiniCourt
 import cv2
 
 
@@ -35,6 +36,9 @@ def main():
         court_keypoints, player_detections
     )
 
+    # Mini Court
+    mini_court = MiniCourt(video_frames[0])
+
     # Draw output
     ## Draw bounding boxes around players
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
@@ -46,6 +50,9 @@ def main():
     output_video_frames = court_line_detector.draw_keypoints_on_video(
         output_video_frames, court_keypoints
     )
+
+    ## Draw mini court
+    output_video_frames = mini_court.draw_mini_court(output_video_frames)
 
     ## Draw frame number on top left corner
     for i, frame in enumerate(output_video_frames):
---
+++ mini_court/mini_court.py
@@ -0,0 +1,164 @@
+import cv2
+import numpy as np
+import sys
+
+sys.path.append("../")
+import constants
+
+from utils import convert_meters_to_pixel_distance, convert_pixel_distance_to_meters
+
+
+class MiniCourt:
+    def __init__(self, frame):
+        self.drawing_rectangle_width = 250
+        self.drawing_rectangle_height = 450
+        self.buffer = 50
+        self.padding_court = 20
+
+        self.set_canvas_background_box_position(frame)
+        self.set_mini_court_position()
+        self.set_court_drawing_keypoints()
+        self.set_court_lines()
+
+    def convert_meters_to_pixels(self, meters):
+        return convert_meters_to_pixel_distance(
+            meters, constants.DOUBLE_LINE_WIDTH, self.court_drawing_rectangle_width
+        )
+
+    def set_court_drawing_keypoints(self):
+        drawing_key_points = [0] * 28
+
+        # Point 0
+        drawing_key_points[0], drawing_key_points[1] = (
+            int(self.court_start_x),
+            int(self.court_start_y),
+        )
+        # Point 1
+        drawing_key_points[2], drawing_key_points[3] = (
+            int(self.court_end_x),
+            int(self.court_start_y),
+        )
+        # Point 2
+        drawing_key_points[4] = int(self.court_start_x)
+        drawing_key_points[5] = self.court_start_y + self.convert_meters_to_pixels(
+            constants.HALF_COURT_LINE_HEIGHT * 2
+        )
+        # Point 3
+        drawing_key_points[6] = (
+            drawing_key_points[0] + self.court_drawing_rectangle_width
+        )
+        drawing_key_points[7] = drawing_key_points[5]
+        # Point 4
+        drawing_key_points[8] = drawing_key_points[0] + self.convert_meters_to_pixels(
+            constants.DOUBLE_ALLEY_DIFFERENCE
+        )
+        drawing_key_points[9] = drawing_key_points[1]
+        # Point 5
+        drawing_key_points[10] = drawing_key_points[4] + self.convert_meters_to_pixels(
+            constants.DOUBLE_ALLEY_DIFFERENCE
+        )
+        drawing_key_points[11] = drawing_key_points[5]
+        # Point 6
+        drawing_key_points[12] = drawing_key_points[2] - self.convert_meters_to_pixels(
+            constants.DOUBLE_ALLEY_DIFFERENCE
+        )
+        drawing_key_points[13] = drawing_key_points[3]
+        # Point 7
+        drawing_key_points[14] = drawing_key_points[6] - self.convert_meters_to_pixels(
+            constants.DOUBLE_ALLEY_DIFFERENCE
+        )
+        drawing_key_points[15] = drawing_key_points[7]
+        # Point 8
+        drawing_key_points[16] = drawing_key_points[8]
+        drawing_key_points[17] = drawing_key_points[9] + self.convert_meters_to_pixels(
+            constants.NO_MANS_LAND_HEIGHT
+        )
+        # Point 9
+        drawing_key_points[18] = drawing_key_points[16] + self.convert_meters_to_pixels(
+            constants.SINGLE_LINE_WIDTH
+        )
+        drawing_key_points[19] = drawing_key_points[17]
+        # Point 10
+        drawing_key_points[20] = drawing_key_points[10]
+        drawing_key_points[21] = drawing_key_points[11] - self.convert_meters_to_pixels(
+            constants.NO_MANS_LAND_HEIGHT
+        )
+        # Point 11
+        drawing_key_points[22] = drawing_key_points[20] + self.convert_meters_to_pixels(
+            constants.SINGLE_LINE_WIDTH
+        )
+        drawing_key_points[23] = drawing_key_points[21]
+        # Point 12
+        drawing_key_points[24] = int(
+            (drawing_key_points[16] + drawing_key_points[18]) / 2
+        )
+        drawing_key_points[25] = drawing_key_points[17]
+        # Point 13
+        drawing_key_points[26] = int(
+            (drawing_key_points[20] + drawing_key_points[22]) / 2
+        )
+        drawing_key_points[27] = drawing_key_points[21]
+
+        self.drawing_key_points = drawing_key_points
+
+    def set_court_lines(self):
+        self.lines = [
+            (0, 2),
+            (4, 5),
+            (6, 7),
+            (1, 3),
+            (0, 1),
+            (8, 9),
+            (10, 11),
+            (10, 11),
+            (2, 3),
+        ]
+
+    def set_mini_court_position(self):
+        self.court_start_x = self.start_x + self.padding_court
+        self.court_start_y = self.start_y + self.padding_court
+        self.court_end_x = self.end_x - self.padding_court
+        self.court_end_y = self.end_y - self.padding_court
+        self.court_drawing_rectangle_width = self.court_end_x - self.court_start_x
+
+    def set_canvas_background_box_position(self, frame):
+        frame = frame.copy()
+
+        self.end_x = frame.shape[1] - self.buffer
+        self.end_y = self.buffer + self.drawing_rectangle_height
+
+        self.start_x = self.end_x - self.drawing_rectangle_width
+        self.start_y = self.end_y - self.drawing_rectangle_height
+
+    def draw_court(self, frame):
+        for i in range(0, len(self.drawing_key_points), 2):
+            x = int(self.drawing_key_points[i])
+            y = int(self.drawing_key_points[i + 1])
+            cv2.circle(frame, (x, y), 5, (0, 0, 255), -1)
+
+        return frame
+
+    def draw_background_rectangle(self, frame):
+        shapes = np.zeros_like(frame, np.uint8)
+
+        cv2.rectangle(
+            shapes,
+            (self.start_x, self.start_y),
+            (self.end_x, self.end_y),
+            (255, 255, 255),
+            cv2.FILLED,
+        )
+        out = frame.copy()
+        alpha = 0.5
+        mask = shapes.astype(bool)
+        out[mask] = cv2.addWeighted(frame, alpha, shapes, 1 - alpha, 0)[mask]
+
+        return out
+
+    def draw_mini_court(self, frames):
+        output_frames = []
+        for frame in frames:
+            frame = self.draw_background_rectangle(frame)
+            frame = self.draw_court(frame)
+            output_frames.append(frame)
+        return output_frames
